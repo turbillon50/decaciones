bd703121dace6099c763312b4f192d97a0b34638
turbillon50 <turbillon50@gmail.com>
2026-06-02 03:01:25 +0000

    Configure Vercel Next deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,8 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".vercel/**",
+    "node_modules/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
